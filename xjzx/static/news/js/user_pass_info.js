9a06816280f0bd909b7b5c1d1689353eca729f6e
wxq <1798787619@qq.com>
2018-06-17 09:51:50 +0800

    aa

--- xjzx/static/news/js/user_pass_info.js
+++
@@ -1,9 +0,0 @@
-function getCookie(name) {
-    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
-    return r ? r[1] : undefined;
-}
-
-
-$(function () {
-
-})
--- xjzx/views_news.py
+++
@@ -1,7 +0,0 @@
-from  flask import  Blueprint
-from flask import render_template
-
-news_blueprint=Blueprint('news',__name__)
-@news_blueprint.route('/')
-def index():
-    return render_template('news/index.html')
